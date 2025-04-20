7ea32ca73e0b4c25051702797df3b46eeae63d9d
dixit1 <dixitnparmar9265@gmail.com>
2025-04-20 13:19:16 +0530

    remove lodash debounce

--- src/app/components/customer-search.tsx
+++ src/app/components/customer-search.tsx
@@ -4,7 +4,7 @@ import type React from "react"
 
 import { useState, useEffect, useMemo } from "react"
 import { Autocomplete, TextField, CircularProgress } from "@mui/material"
-import debounce from "lodash/debounce"
+import {debounce} from "lodash"
 import type { Customer } from "../types"
 
 interface CustomerSearchProps {
